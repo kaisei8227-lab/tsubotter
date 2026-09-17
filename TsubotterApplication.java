5596ccecf207ea580f7ea24fa560bccec40f01e2
kaisei8227-lab <kaisei8227@gmail.com>
2026-09-17 15:39:41 +0900

    Add files via upload

--- TsubotterApplication.java
+++ TsubotterApplication.java
@@ -2,10 +2,12 @@ package com.example.tsubotter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan; // ★追加
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example") // ★この行を追加（com.example 以下の全コントローラーを強制検出）
+@ComponentScan(basePackages = "com.example")
+@EntityScan(basePackages = "com.example")
 public class TsubotterApplication {
 
     public static void main(String[] args) {
